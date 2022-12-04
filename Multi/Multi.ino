64c20a16a7da18264ae8a0e21610da85f4b92268
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-12-04 00:08:32 -0600

    Update Multi.ino

--- Multi/Multi.ino
+++ Multi/Multi.ino
@@ -8,9 +8,9 @@
 
 FASTLED_USING_NAMESPACE
 
-#define NUM_STRIPS 3
+#define NUM_STRIPS 8
 #define NUM_LEDS_PER_STRIP 64
-CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
+//CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 CRGB leds0[NUM_LEDS_PER_STRIP];
 CRGB leds1[NUM_LEDS_PER_STRIP];
 CRGB leds2[NUM_LEDS_PER_STRIP];
@@ -32,7 +32,7 @@ uint8_t gHue5 = 0;
 uint8_t gHue6 = 0;
 uint8_t gHue7 = 0;
 #define DHTPIN D7//Pin donde se lee el sensor
-const int SENSOR_READ = 500;//Determina el tiempo de lectura del sensor a 1 segundo
+const int SENSOR_READ = 250;//Determina el tiempo de lectura del sensor a 1 segundo
 const int LECTURE_THRESHOLD = 3; //Detecta un 10% de cambio en la humedad
 int avg;
 
@@ -44,6 +44,8 @@ int humidity [16];//Arreglo de anillo
 //Constante de la bilbioteca que selecciona el modelo de sensor que usamos
 #define DHTTYPE DHT11   // DHT 11
 
+int pos;
+
 DHT dht(DHTPIN, DHTTYPE);
 // For mirroring strips, all the "special" stuff happens just in setup.  We
 // just addLeds multiple times, once for each strip
@@ -52,14 +54,14 @@ void setup() {
   FastLED.addLeds<LED_TYPE,D0,COLOR_ORDER>(leds0, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
   FastLED.addLeds<LED_TYPE,D1,COLOR_ORDER>(leds1, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
   FastLED.addLeds<LED_TYPE,D2,COLOR_ORDER>(leds2, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE,D3,COLOR_ORDER>(leds2, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE,D4,COLOR_ORDER>(leds2, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE,D5,COLOR_ORDER>(leds2, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE,D6,COLOR_ORDER>(leds2, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE,D8,COLOR_ORDER>(leds2, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,D3,COLOR_ORDER>(leds3, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,D4,COLOR_ORDER>(leds4, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,D5,COLOR_ORDER>(leds5, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,D6,COLOR_ORDER>(leds6, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,D8,COLOR_ORDER>(leds7, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
 
-  Serial.begin (115200);
-  Serial.println ("Inicio");
+  //Serial.begin (115200);
+  //Serial.println ("Inicio");
 
   dht.begin();
   //Inicia secuencia de control de tiempos no bloqueante
@@ -87,10 +89,10 @@ void loop() {
     }
     avg = avg/16;
 
-    Serial.print ("avg ");
-    Serial.print (avg);
-    Serial.print (" hum ");
-    Serial.println (humidity[ring]);
+    //Serial.print ("avg ");
+    //Serial.print (avg);
+    //Serial.print (" hum ");
+    //Serial.println (humidity[ring]);
 
     if ((humidity [ring] - avg) > LECTURE_THRESHOLD) {
       doTheEvolutionBaby ();//Aqui es donde hay que poner el detonador de generaciones
@@ -107,7 +109,7 @@ void loop() {
   // send the 'leds' array out to the actual LED strip
   FastLED.show();  
   // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
+  FastLED.delay(5000/FRAMES_PER_SECOND); 
 
   // do some periodic updates
   //EVERY_N_MILLISECONDS( 5000 ) { gHue += random8(255); }
@@ -116,30 +118,30 @@ void loop() {
 void confetti() 
 {
   // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds0, NUM_LEDS_PER_STRIP, 5);
-  fadeToBlackBy( leds1, NUM_LEDS_PER_STRIP, 5);
-  fadeToBlackBy( leds2, NUM_LEDS_PER_STRIP, 5);
-  fadeToBlackBy( leds3, NUM_LEDS_PER_STRIP, 5);
-  fadeToBlackBy( leds4, NUM_LEDS_PER_STRIP, 5);
-  fadeToBlackBy( leds5, NUM_LEDS_PER_STRIP, 5);
-  fadeToBlackBy( leds6, NUM_LEDS_PER_STRIP, 5);
-  fadeToBlackBy( leds7, NUM_LEDS_PER_STRIP, 5);
-  int pos = random16(NUM_LEDS_PER_STRIP);
-  leds0[pos] += CHSV( gHue0 + random8(16), 200, 255);
+  fadeToBlackBy( leds0, NUM_LEDS_PER_STRIP, 40);
+  fadeToBlackBy( leds1, NUM_LEDS_PER_STRIP, 40);
+  fadeToBlackBy( leds2, NUM_LEDS_PER_STRIP, 40);
+  fadeToBlackBy( leds3, NUM_LEDS_PER_STRIP, 40);
+  fadeToBlackBy( leds4, NUM_LEDS_PER_STRIP, 40);
+  fadeToBlackBy( leds5, NUM_LEDS_PER_STRIP, 40);
+  fadeToBlackBy( leds6, NUM_LEDS_PER_STRIP, 40);
+  fadeToBlackBy( leds7, NUM_LEDS_PER_STRIP, 40);
   pos = random16(NUM_LEDS_PER_STRIP);
-  leds1[pos] += CHSV( gHue1 + random8(16), 200, 255);
+  leds0[pos] += CHSV( gHue0 + random8(32), 200, 255);
   pos = random16(NUM_LEDS_PER_STRIP);
-  leds2[pos] += CHSV( gHue2 + random8(16), 200, 255);
+  leds1[pos] += CHSV( gHue1 + random8(32), 200, 255);
   pos = random16(NUM_LEDS_PER_STRIP);
-  leds3[pos] += CHSV( gHue3 + random8(16), 200, 255);
+  leds2[pos] += CHSV( gHue2 + random8(32), 200, 255);
   pos = random16(NUM_LEDS_PER_STRIP);
-  leds4[pos] += CHSV( gHue4 + random8(16), 200, 255);
+  leds3[pos] += CHSV( gHue3 + random8(32), 200, 255);
   pos = random16(NUM_LEDS_PER_STRIP);
-  leds5[pos] += CHSV( gHue5 + random8(16), 200, 255);
+  leds4[pos] += CHSV( gHue4 + random8(32), 200, 255);
   pos = random16(NUM_LEDS_PER_STRIP);
-  leds6[pos] += CHSV( gHue6 + random8(16), 200, 255);
+  leds5[pos] += CHSV( gHue5 + random8(32), 200, 255);
   pos = random16(NUM_LEDS_PER_STRIP);
-  leds7[pos] += CHSV( gHue7 + random8(16), 200, 255);
+  leds6[pos] += CHSV( gHue6 + random8(32), 200, 255);
+  pos = random16(NUM_LEDS_PER_STRIP);
+  leds7[pos] += CHSV( gHue7 + random8(32), 200, 255);
   //addGlitter(10);
 }
 
@@ -153,5 +155,11 @@ void doTheEvolutionBaby () {
   gHue5 += random8(64);
   gHue6 += random8(64);
   gHue7 += random8(64);
-  gHue2 += random8(64);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds6[ random16(NUM_LEDS_PER_STRIP) ] += CRGB::White;
+  }
 }
